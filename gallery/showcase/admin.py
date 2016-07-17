df5b75cb2efdd7e14b37a4b36a744cc01dc63941
Zachary King <kingzach77@gmail.com>
2016-07-17 18:00:52 -0500

    Updated models and admin layout

--- gallery/showcase/admin.py
+++ gallery/showcase/admin.py
@@ -1,10 +1,29 @@
 from django.contrib import admin
 
-from .models import ArtPiece
+from .models import ArtPiece, Gallery
 
+
+class ArtPieceInline(admin.TabularInline):
+    fields = ['title', 'artist', 'image', 'gallery', 'pub_date']
+    model = ArtPiece
+    extra = 1
+
+
+class GalleryAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['name']}),
+    ]
+    inlines = [ArtPieceInline]
+    list_display = ('name',)
+    search_fields = ['name',]
 
 class ArtPieceAdmin(admin.ModelAdmin):
-    fields = ['title', 'artist', 'image', 'pub_date']
+    fieldsets = [
+        (None, {'fields': ['title', 'artist', 'image', 'gallery', 'pub_date']}),
+    ]
+    list_filter = ['artist', 'pub_date']
+    search_fields = ['title', 'artist']
 
 
+admin.site.register(Gallery, GalleryAdmin)
 admin.site.register(ArtPiece, ArtPieceAdmin)
--- gallery/showcase/models.py
+++ gallery/showcase/models.py
@@ -4,19 +4,22 @@ from django.utils import timezone
 import datetime
 
 
+class Gallery(models.Model):
+    """
+    Contains art pieces
+    """
+    name = models.CharField(max_length=200, default='')
+
+    def __str__(self):
+        return self.name
+
+
 class ArtPiece(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateField('date published')
     image = models.ImageField()
     artist = models.CharField(max_length=100)
-    gallery = models.ForeignKey('Gallery')
+    gallery = models.ForeignKey('Gallery', default=0)
 
     def __str__(self):
         return self.title
-
-
-class Gallery(models.Model):
-    """
-    Contains art pieces
-    """
-    pass
